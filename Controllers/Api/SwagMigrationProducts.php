38d6bd112e769aa67e87aec6879786d950c2aea4
Holger-Thomas Kaßner <h.kassner@shopware.com>
2018-07-27 09:35:12 +0200

    Add default shop language to product resultset

--- Controllers/Api/SwagMigrationProducts.php
+++ Controllers/Api/SwagMigrationProducts.php
@@ -1,5 +1,7 @@
 <?php
 
+use Shopware\Models\Shop\Shop;
+
 class Shopware_Controllers_Api_SwagMigrationProducts extends Shopware_Controllers_Api_Rest
 {
     public function indexAction()
@@ -9,7 +11,13 @@ class Shopware_Controllers_Api_SwagMigrationProducts extends Shopware_Controller
         $productService = $this->container->get('swag_migration_api.service.product_service');
 
         $products = $productService->getProducts($offset, $limit);
+        /** @var Shop $defaultShop */
+        $defaultShop = $this->container->get('models')->getRepository(Shop::class)->getDefault();
 
-        $this->view->assign(['success' => true, 'data' => $products]);
+        $this->view->assign([
+            'success' => true,
+            'data' => $products,
+            'default_locale' => $defaultShop->getLocale()->getLocale()
+        ]);
     }
 }
