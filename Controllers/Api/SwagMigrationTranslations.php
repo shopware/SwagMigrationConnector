6cefd759ec1f1033ad5b0c86ea3d35b0936b4d83
Holger-Thomas Kaßner <h.kassner@shopware.com>
2018-08-06 13:13:37 +0200

    Force SSL and check for admin rights - Added new exceptions

--- Controllers/Api/SwagMigrationTranslations.php
+++ Controllers/Api/SwagMigrationTranslations.php
@@ -4,8 +4,41 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+use Shopware\Models\User\Role;
+use SwagMigrationApi\Exception\PermissionDeniedException;
+use SwagMigrationApi\Exception\UnsecureRequestException;
+
+/**
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 class Shopware_Controllers_Api_SwagMigrationTranslations extends Shopware_Controllers_Api_Rest
 {
+    public function preDispatch()
+    {
+        parent::preDispatch();
+
+        if (!$this->Request()->isSecure()) {
+            throw new UnsecureRequestException(
+                'SSL required',
+                426
+            );
+        }
+
+        if ($this->container->initialized('Auth')) {
+            /** @var Role $role */
+            $role = $this->container->get('Auth')->getIdentity()->role;
+
+            if (!$role->getAdmin()) {
+                throw new PermissionDeniedException(
+                    'Permission denied. API user does not have sufficient rights for this action.',
+                    401
+                );
+            }
+        }
+    }
+
     public function indexAction()
     {
         $offset = (int) $this->Request()->getParam('offset', 0);
