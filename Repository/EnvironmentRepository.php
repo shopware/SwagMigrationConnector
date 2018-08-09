3ba593a0a1698850d29cdfd69ff2e299f1202ddd
Holger-Thomas Kaßner <h.kassner@shopware.com>
2018-08-09 16:47:22 +0200

    PT-9435 - Add endpoint for general environment information

---
+++ Repository/EnvironmentRepository.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwagMigrationApi\Repository;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\DBAL\Schema\Column;
+
+class EnvironmentRepository
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @param Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @param string $table
+     *
+     * @return int
+     */
+    public function getTableCount($table)
+    {
+        $querybuilder = $this->connection->createQueryBuilder();
+
+        return (int) $querybuilder->select('COUNT(id)')
+            ->from($table)
+            ->execute()
+            ->fetchColumn()
+        ;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCategoryCount()
+    {
+        $querybuilder = $this->connection->createQueryBuilder();
+
+        return (int) $querybuilder->select('COUNT(id)')
+            ->from('s_categories')
+            ->where('path IS NOT NULL')
+            ->andWhere('parent IS NOT NULL')
+            ->execute()
+            ->fetchColumn()
+        ;
+    }
+
+    /**
+     * @return array
+     */
+    public function getShops()
+    {
+        $query = $this->connection->createQueryBuilder();
+
+        $query->from('s_core_shops', 'shop');
+        $query->addSelect('shop.id as identifier');
+        $this->addTableSelection($query, 's_core_shops', 'shop');
+
+        $query->leftJoin('shop', 's_core_locales', 'locale', 'shop.locale_id = locale.id');
+        $this->addTableSelection($query, 's_core_locales', 'locale');
+
+        $query->orderBy('shop.main_id');
+
+        return $query->execute()->fetchAll(\PDO::FETCH_GROUP | \PDO::FETCH_UNIQUE);
+    }
+
+    /**
+     * @param QueryBuilder $query
+     * @param string       $table
+     * @param string       $tableAlias
+     */
+    private function addTableSelection(QueryBuilder $query, $table, $tableAlias)
+    {
+        $columns = $this->connection->getSchemaManager()->listTableColumns($table);
+
+        /** @var Column $column */
+        foreach ($columns as $column) {
+            $selection = str_replace(
+                ['#tableAlias#', '#column#'],
+                [$tableAlias, $column->getName()],
+                '`#tableAlias#`.`#column#` as `#tableAlias#.#column#`'
+            );
+
+            $query->addSelect($selection);
+        }
+    }
+}
---
+++ Service/EnvironmentService.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwagMigrationApi\Service;
+
+use Shopware\Components\Model\ModelManager;
+use Shopware\Models\Shop\Shop;
+use SwagMigrationApi\Repository\EnvironmentRepository;
+
+class EnvironmentService extends AbstractApiService
+{
+    const TABLES_TO_COUNT = [
+        'products' => 's_articles_details',
+        'customers' => 's_user',
+        'categories' => 's_categories',
+        'assets' => 's_media',
+        'orders' => 's_order',
+        'shops' => 's_core_shops',
+        'shoppingWorlds' => 's_emotion',
+    ];
+
+    /**
+     * @var ModelManager
+     */
+    private $modelManager;
+
+    /**
+     * @var EnvironmentRepository
+     */
+    private $repository;
+
+    /**
+     * @var string
+     */
+    private $version;
+
+    /**
+     * @var string
+     */
+    private $versionText;
+
+    /**
+     * @var string
+     */
+    private $revision;
+
+    /**
+     * @param ModelManager $modelManager
+     * @param string       $version
+     * @param string       $versionText
+     * @param string       $revision
+     */
+    public function __construct(
+        ModelManager $modelManager,
+        EnvironmentRepository $environmentRepository,
+        $version,
+        $versionText,
+        $revision
+    ) {
+        $this->modelManager = $modelManager;
+        $this->repository = $environmentRepository;
+        $this->version = $version;
+        $this->versionText = $versionText;
+        $this->revision = $revision;
+    }
+
+    /**
+     * @return array
+     */
+    public function getEnvironmentInformation()
+    {
+        /** @var Shop $defaultShop */
+        $defaultShop = $this->modelManager->getRepository(Shop::class)->getDefault();
+
+        // represents the main language of the migrated shop
+        $locale = $defaultShop->getLocale()->getLocale();
+
+        $resultSet = [
+            'defaultShopLanguage' => $locale,
+            'shopwareVersion' => $this->version,
+            'versionText' => $this->versionText,
+            'revision' => $this->revision,
+            'strucure' => $this->getShopStructure(),
+        ];
+
+        foreach (self::TABLES_TO_COUNT as $key => $table) {
+            if ($key === 'categories') {
+                $resultSet[$key] = $this->repository->getCategoryCount();
+                continue;
+            }
+            $resultSet[$key] = $this->repository->getTableCount($table);
+        }
+
+        return $resultSet;
+    }
+
+    /**
+     * @return array
+     */
+    private function getShopStructure()
+    {
+        $fetchedShops = $this->repository->getShops();
+        $shops = $this->mapData($fetchedShops, [], ['shop']);
+
+        foreach ($shops as $key => &$shop) {
+            if (!empty($shop['main_id'])) {
+                $shops[$shop['main_id']]['children'][] = $shop;
+                unset($shops[$key]);
+            }
+        }
+
+        return array_values($shops);
+    }
+}
