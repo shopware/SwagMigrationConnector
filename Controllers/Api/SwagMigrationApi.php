22c3484247ec54c930f93086d92f3e7b75418f89
Holger-Thomas Kaßner <h.kassner@shopware.com>
2018-09-18 15:59:08 +0200

    PT-9640 - Optimize request authorization

---
+++ Controllers/Api/SwagMigrationApi.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Shopware\Models\User\Role;
+use SwagMigrationApi\Exception\PermissionDeniedException;
+use SwagMigrationApi\Exception\UnsecureRequestException;
+
+/**
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Shopware_Controllers_Api_SwagMigrationApi extends Shopware_Controllers_Api_Rest
+{
+    /**
+     * @throws PermissionDeniedException
+     * @throws UnsecureRequestException
+     */
+    public function preDispatch()
+    {
+        parent::preDispatch();
+
+        if (!$this->Request()->isSecure()) {
+            throw new UnsecureRequestException(
+                'SSL required',
+                426
+            );
+        }
+
+        if ($this->container->initialized('Auth')) {
+            /** @var Role $role */
+            $role = $this->container->get('Auth')->getIdentity()->role;
+
+            if ($role->getAdmin()) {
+                return;
+            }
+        }
+
+        throw new PermissionDeniedException(
+            'Permission denied. API user does not have sufficient rights for this action or could not be authenticated.',
+            401
+        );
+    }
+}
